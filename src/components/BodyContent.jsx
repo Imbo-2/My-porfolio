1ece94fa70e296cd4657728d7e6a87b798be5a9e
Dwight02 <iiimbo.student@asiancollege.edu.ph>
2025-08-16 22:41:03 +0800

    Update BodyContent.jsx

--- src/components/BodyContent.jsx
+++ src/components/BodyContent.jsx
@@ -6,15 +6,16 @@ function BodyContent() {
       <h2>About Me</h2>
       <p>I’m a web developer passionate about building beautiful user experiences.</p>
 
-      <h2>My Skills</h2>
+      <h2>Bio</h2>
       <ul>
-        <li>HTML, CSS, JavaScript</li>
-        <li>React.js</li>
-        <li>Git & GitHub</li>
+        <li>I am a 20-year-old Filipino, born on December 02, 2004. I am currently studying at Asian College of Science and Technology, taking up Information Technology.</li>
+        <h2>Skill</h2>
+        <li>I have experience with HTML, CSS, React, and Node.js. I am also familiar with Git and GitHubfor version control.</li>
       </ul>
     </section>
   );
 }
 
 export default BodyContent;
+
 
